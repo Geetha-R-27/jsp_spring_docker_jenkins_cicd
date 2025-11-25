d13660feddede9b56ce0b20db170726df5c6b0b7
Geetha R <geethar272003@gmail.com>
2025-11-25 14:43:43 +0530

    Update EmailSender.java

--- src/main/java/org/jsp/jsp_gram/helper/EmailSender.java
+++ src/main/java/org/jsp/jsp_gram/helper/EmailSender.java
@@ -12,30 +12,32 @@ import jakarta.mail.internet.MimeMessage;
 @Component
 public class EmailSender {
 
-	@Autowired
-	JavaMailSender mailSender;
+    @Autowired
+    JavaMailSender mailSender;
 
-	@Autowired
-	TemplateEngine engine;
+    @Autowired
+    TemplateEngine engine;
 
-	public void sendOtp(String to, int otp, String name) {
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(message);
+    public void sendOtp(String to, int otp, String name) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-		try {
-			helper.setFrom("geethar272003@gmail.com", "JspGram Application");
-			helper.setTo(to);
-			helper.setSubject("Verify Your Email thru OTP");
+            helper.setFrom("geethar272003@gmail.com", "JspGram Application");
+            helper.setTo(to);
+            helper.setSubject("Verify Your Email through OTP");
 
-			Context context = new Context();
-			context.setVariable("name", name);
-			context.setVariable("otp", otp);
+            Context context = new Context();
+            context.setVariable("name", name);
+            context.setVariable("otp", otp);
 
-			String body = engine.process("otp-template.html", context);
-			helper.setText(body, true);
-		} catch (Exception e) {
-		}
-		mailSender.send(message);
-	}
+            String body = engine.process("otp-template.html", context);
+            helper.setText(body, true);
 
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
